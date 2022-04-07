df0b99b0b15ba2cdee77757aaaeabdb5ea07d649
MAYA <kurihara-maya@keio.jp>
2022-04-07 23:57:08 +0900

    。

--- .pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/trng_api.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/trng_api.o: \
- /Users/maya/.platformio/packages/framework-mbed/targets/TARGET_STM/trng_api.c \
- .pio/build/nucleo_f303k8/mbed_config.h
